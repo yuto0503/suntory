a2a42a25db725aac8ff1d4697e2d7f39f1a6d67e
‘yuto’_0503 <y.yuto.0503@gmail.comgit config --global user.name yuto_0503git config --global user.email y.yuto.0503@gmail.com>
2023-04-10 09:32:14 +0900

    初回コミット

---
+++ js/comon.js
@@ -0,0 +1,7 @@
+// ページ内リンク
+$('#page-link a[href*="#"]').click(function () {
+  var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+  var pos = $(elmHash).offset().top-70;//idの上部の距離からHeaderの高さを引いた値を取得
+  $('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+  return false;
+});
